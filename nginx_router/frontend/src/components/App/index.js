c2acaa73cc7fbd42fa634c01576fed72efa0b51c
Bennett Dixon <benbendixon@gmail.com>
2019-10-06 11:12:03 -0700

    app is now uidcontext provider, should be changed in PROD

--- nginx_router/frontend/src/components/App/index.js
+++ nginx_router/frontend/src/components/App/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { UidContext } from "../UidContext";
 
 import Navigation from "../Navigation";
 import LandingPage from "../Landing";
@@ -7,14 +9,18 @@ import SleepChartPage from "../Sleep";
 import * as ROUTES from "../../constants/routes";
 
 class App extends Component {
+  // TODO uidContext should be included in authentication scheme
+  // current implementation is hardcoded id in App
   render() {
     return (
       <Router>
-        <div>
-          <Navigation />
-          <Route exact path={ROUTES.LANDING} component={LandingPage} />
-          <Route path={ROUTES.SLEEP_CHARTS} component={SleepChartPage} />
-        </div>
+        <UidContext.Provider value={1}>
+          <div>
+            <Navigation />
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.SLEEP_CHARTS} component={SleepChartPage} />
+          </div>
+        </UidContext.Provider>
       </Router>
     );
   }
