57761c756ebc54771b4b4149c8442cd378448385
Bennett Dixon <benbendixon@gmail.com>
2019-10-06 19:03:28 -0700

    now properly detecting singular objects

--- nginx_router/frontend/src/components/TimeText/DateRange.js
+++ nginx_router/frontend/src/components/TimeText/DateRange.js
@@ -2,15 +2,19 @@ import React from "react";
 
 import TimeLabel from "./TimeLabel";
 
+import SleepTime from "../../models/SleepTime";
+
 import "./DateRange.css";
 
 const DateRange = ({ sleepTimes }) => {
-  if (typeof sleepTimes !== typeof array) {
+  if (sleepTimes.length === undefined) {
+    // not an array, render singular object
     console.log("date range not array");
   }
+  const dateRange = SleepTime.getDateRangeText(sleepTimes);
   return (
     <div className="date-range">
-      <TimeLabel label={"Date range"} fontSize={20} />
+      <TimeLabel label={dateRange} fontSize={20} />
     </div>
   );
 };
